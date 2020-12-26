3818c2a23622e67f07b6d7ce36821c5a11efb9b3
Yudai YAMAMOTO <kohizeri0804@gmail.com>
2020-12-26 19:13:23 +0900

    画像の保存

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
-from flask import Flask, render_template, request,redirect,session
+from flask import Flask, render_template, request, redirect, session
 from datetime import datetime
-app = Flask(__name__) 
+import os
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def get_home():
+    return render_template('index.html')
+
+@app.route('/', methods=["post"])
+def get_face():
+    # ファイル容量を1MBに制限
+    app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
+
+    upload = request.files['upload']
+    if not upload.filename.lower().endswitch(('.png', 'jpg', 'jpeg')):
+        return 'png, jpg, jpeg形式のファイルを選択してください。'
+
+    save_path = get_save_path()
+    print(save_path)
+    filename = upload.filename
+    print(filename)
+    upload.save(os.path.join(save_path, filename))
+
+    return redirect('/')
+
+def get_save_path():
+    path_dir = "./static/img"
+    return path_dir
+
 
 # flaskアプリを動かすための記述
 if __name__ == "__main__":
