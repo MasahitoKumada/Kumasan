d0c6658e63799997cfe1ed6f41725165802e5ffd
DESKTOP-M8GV6D6\Miyuu <weekender.nyanko@gmail.com>
2020-12-26 20:33:02 +0900

    create html/css

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request,redirect,session
 from datetime import datetime
 app = Flask(__name__) 
 
+@app.route("/", methods = ['get'])
+def get_home():
+    return render_template('index.html')
+
 # flaskアプリを動かすための記述
 if __name__ == "__main__":
     app.run(debug = True)
