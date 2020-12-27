1967901746478f530b121bd4c10f049034e499f4
DESKTOP-M8GV6D6\Miyuu <weekender.nyanko@gmail.com>
2020-12-27 14:48:49 +0900

    create newpages

---
+++ static/javascript.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    const spinner = document.getElementById('loading');
+    spinner.classList.add('loaded');
+  }
+
+  function goClick1() {
+    // クリック時の動作を指定できます
+    location.href = "https://www.youtube.com/";
+  }
+
+  function goClick2() {
+    // クリック時の動作を指定できます
+    location.href = "https://twitter.com/home";
+  }
